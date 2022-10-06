a11d06686192b7d5590083931ce51d6292a5c190
Shushant Kumar <s.living.hell@gmail.com>
2022-10-07 00:17:25 +0530

    Adding script for  PHP Validaton, password encryption and storing data from contact form to database

--- register.php
+++ register.php
@@ -28,6 +28,78 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Script -->
+	<script type="text/javascript">
+        $(document).ready(function () {
+
+            $("#register").click(function () {
+
+                fname = $("#fname").val();
+                lname = $("#lname").val();
+                email = $("#email").val();
+                password = $("#password").val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "adduser.php",
+                    data: "fname=" + fname + "&lname=" + lname + "&email=" + email + "&password=" + password,
+                    success: function (html) {
+                        if (html == 'true') {
+
+                            $("#add_err2").html('<div class="alert alert-success"> \
+                                                 <strong>Account</strong> processed. \ \
+                                                 </div>');
+
+                            window.location.href = "index.php";
+
+                        } else if (html == 'false') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> already in system. \ \
+                                                 </div>');                    
+
+                        } else if (html == 'fname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>First Name</strong> is required. \ \
+                                                 </div>');
+												 
+						} else if (html == 'lname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Last Name</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eformat') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> format is not valid. \ \
+                                                 </div>');
+												 
+						} else if (html == 'pshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Password</strong> must be at least 4 characters . \ \
+                                                 </div>');
+
+                        } else {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Error</strong> processing request. Please try again. \ \
+                                                 </div>');
+                        }
+                    },
+                    beforeSend: function () {
+                        $("#add_err2").html("loading...");
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
+
 </head>
 
 <body>
@@ -46,6 +118,7 @@
                     <h2 class="intro-text text-center">Registeration
                         <strong>form</strong>
                     </h2>
+                    <div id="add_err2"></div>
                     <hr>
                     <form role="form">
                         <div class="row">
@@ -87,9 +160,6 @@
             </div>
         </div>
     </footer>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
